74d6c62852fb9cb6affe93450bf4bfa7209d432c
nahusenayElias <eliasars@yahoo.com>
2026-01-30 14:47:45 +0200

    Implement backend + frontend connection

---
+++ frontend/app/admin/page.tsx
@@ -0,0 +1,9 @@
+// app/admin/page.tsx
+export default function AdminPage() {
+    return (
+      <div className="max-w-7xl mx-auto p-8">
+        <h1 className="text-3xl font-bold mb-4">Admin Dashboard</h1>
+        <p>Coming soon...</p>
+      </div>
+    );
+  }
